ddfffb315ad5bfd07bebe223ca222d0a0f5cd8fa
__prav__ <pravasith@gmail.com>
2026-06-16 13:30:28 -0400

    feat: before packaging engine

--- Coffee/include/Coffee/Core/Window.h
+++ Coffee/include/Coffee/Core/Window.h
@@ -1,10 +1,40 @@
 #pragma once
 
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 namespace Core {
 class Window {
 public:
-  Window() { std::cout << "WINDOW CREATED" << '\n'; }
+  Window() {
+    std::cout << "WINDOW CREATED" << '\n';
+
+    GLFWwindow *window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+      std::cout << "Error initing glfw" << '\n';
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+      glfwTerminate();
+      // throw err
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+      /* Swap front and back buffers */
+      glfwSwapBuffers(window);
+
+      /* Poll for and process events */
+      glfwPollEvents();
+    }
+
+    glfwTerminate();
+  }
 };
 } // namespace Core
--- GameApp/game.cpp
+++ GameApp/game.cpp
@@ -1,9 +1,10 @@
 #include "Coffee/Core/Application.h"
+
 #include <iostream>
 #include <ostream>
 
 int main() {
-    std::cout << "I'm inside game application" << std::endl;
+  std::cout << "I'm inside game application" << std::endl;
   Core::Application &appInstance = Core::Application::Get();
 
   return 0;
